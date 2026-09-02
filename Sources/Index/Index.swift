fc4d350908ed06fd18501709e50fc26464e56aa8
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-02 09:54:41 +0200

    Collapse the Index typealias onto one line

--- Sources/Index/Index.swift
+++ Sources/Index/Index.swift
@@ -2,5 +2,4 @@ public import Ordinal
 public import Ordinal_Protocol
 public import Tagged
 
-public typealias Index<Element: ~Copyable & ~Escapable> =
-    Tagged::Tagged<Element, Ordinal::Ordinal>
+public typealias Index<Element: ~Copyable & ~Escapable> = Tagged::Tagged<Element, Ordinal::Ordinal>
